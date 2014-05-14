29c3d7fa1cb44d1722f3b92ce4ac4c4ac9f13f4e
dengliuiang <dengliujiang@chinacloud.com.cn>
2014-05-14 11:00:17 +0800

    调整绘图运行线数据结构

--- src/main/webapp/assets/js/trainplan/train_runline_data.js
+++ src/main/webapp/assets/js/trainplan/train_runline_data.js
@@ -16,23 +16,16 @@ for(var i=0;i<2;i++) {
  * x\y坐标请求接口返回数据格式
  */
 var gridData = {
-		code : "0",	//非0即接口内部异常
-		message:"",
-		data:{
 			days:dateArray,//[{runDate:"20140510",runDateText:"2014-05-10"},{runDate:"20140511",runDateText:"2014-05-11"}]
 			crossStns:[{stnName:"成都"},{stnName:"遂宁"},{stnName:"南充"},{stnName:"蓬安"},{stnName:"营山"},{stnName:"土溪"},{stnName:"达州"},{stnName:"安康"},{stnName:"华山"},{stnName:"北京西"}]
-		}
 };
 
 
 /*==================================================
  * 单个车次运行线请求接口返回数据格式
  */
-var trainRunLineCanvasData = {
-		code : "0",	//非0即接口内部异常
-		message:"",
-		data:
-			{
+var trainRunLineCanvasData = 
+			[{
 				  trainName:"K818",
 				  startStn:"成都",//始发站名 为null时填入""（空串）
 				  endStn:"北京西",//终到站名为null时填入""（空串）
@@ -47,15 +40,11 @@ var trainRunLineCanvasData = {
 						{runDays:1,stnName:"华山",arrTime:"2014-05-11 07:45",dptTime:"2014-05-11 07:51",stayTime:6},
 						{runDays:1,stnName:"北京西",arrTime:"2014-05-11 21:07",dptTime:"2014-05-11 21:07",stayTime:0}
 				  ]
-			}
-};
+			}];
 
 //模拟 靳磊 接口返回数据
-var jl_trainRunLineCanvasData = {
-		code : "0",	//非0即接口内部异常
-		message:"",
-		data:
-			{
+var jl_trainRunLineCanvasData = 
+			[{
 				  trainName:"K818",
 				  startStn:"成都",//始发站名 为null时填入""（空串）
 				  endStn:"北京西",//终到站名为null时填入""（空串）
@@ -70,14 +59,10 @@ var jl_trainRunLineCanvasData = {
 						{runDays:1,stnName:"华山",arrTime:"2014-05-11 07:45",dptTime:"2014-05-11 07:51",stayTime:6},
 						{runDays:1,stnName:"北京西",arrTime:"2014-05-11 21:07",dptTime:"2014-05-11 21:07",stayTime:0}
 				  ]
-			}
-};
+			}];
 //模拟 靳磊 接口返回数据    错误数据
-var jl_error_trainRunLineCanvasData = {
-		code : "0",	//非0即接口内部异常
-		message:"",
-		data:
-			{
+var jl_error_trainRunLineCanvasData = 
+			[{
 				  trainName:"K818",
 				  startStn:"成都",//始发站名 为null时填入""（空串）
 				  endStn:"北京西",//终到站名为null时填入""（空串）
@@ -92,8 +77,7 @@ var jl_error_trainRunLineCanvasData = {
 						{runDays:1,stnName:"华山",arrTime:"2014-05-11 07:45",dptTime:"2014-05-11 07:51",stayTime:6},
 						{runDays:1,stnName:"北京西",arrTime:"2014-05-11 21:07",dptTime:"2014-05-11 21:07",stayTime:0}
 				  ]
-			}
-};
+			}];
 
 
 
