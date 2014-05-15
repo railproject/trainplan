c736ca2c073851e25670bb8bc681569f8c5a87da
speeder <speeder179@gmail.com>
2014-05-15 20:26:13 +0800

    login

--- src/main/java/org/railway/com/trainplan/entity/User.java
+++ src/main/java/org/railway/com/trainplan/entity/User.java
@@ -5,94 +5,79 @@
  *******************************************************************************/
 package org.railway.com.trainplan.entity;
 
-import org.apache.commons.lang3.builder.ToStringBuilder;
-import org.hibernate.validator.constraints.Email;
-import org.hibernate.validator.constraints.NotBlank;
-
 /**
  * 用户.
  * 
  * @author calvin
  */
 public class User {
-    private long id;
-	private String loginName;
-	private String plainPassword;
-	private String password;
-	private String salt;
-	private String name;
-	private String email;
-	private String status;
+    private int id;
 
-    public long getId() {
+    private String username;
+
+    private String name;
+
+    private String password;
+
+    private String ljpym;
+
+    private String ljjc;
+
+    private String ljqc;
+
+    public int getId() {
         return id;
     }
 
-    public void setId(long id) {
+    public void setId(int id) {
         this.id = id;
     }
 
-    @NotBlank
-	public String getLoginName() {
-		return loginName;
-	}
+    public String getUsername() {
+        return username;
+    }
 
-	public void setLoginName(String loginName) {
-		this.loginName = loginName;
-	}
+    public void setUsername(String username) {
+        this.username = username;
+    }
 
-	public String getPlainPassword() {
-		return plainPassword;
-	}
+    public String getPassword() {
+        return password;
+    }
 
-	public void setPlainPassword(String plainPassword) {
-		this.plainPassword = plainPassword;
-	}
+    public void setPassword(String password) {
+        this.password = password;
+    }
 
-	public String getPassword() {
-		return password;
-	}
+    public String getLjpym() {
+        return ljpym;
+    }
 
-	public void setPassword(String password) {
-		this.password = password;
-	}
+    public void setLjpym(String ljpym) {
+        this.ljpym = ljpym;
+    }
 
-	public String getSalt() {
-		return salt;
-	}
+    public String getLjjc() {
+        return ljjc;
+    }
 
-	public void setSalt(String salt) {
-		this.salt = salt;
-	}
+    public void setLjjc(String ljjc) {
+        this.ljjc = ljjc;
+    }
 
-	@NotBlank
-	public String getName() {
-		return name;
-	}
+    public String getLjqc() {
+        return ljqc;
+    }
 
-	public void setName(String name) {
-		this.name = name;
-	}
+    public void setLjqc(String ljqc) {
+        this.ljqc = ljqc;
+    }
 
-	@Email
-	public String getEmail() {
-		return email;
-	}
+    public String getName() {
+        return name;
+    }
 
-	public void setEmail(String email) {
-		this.email = email;
-	}
-
-	public String getStatus() {
-		return status;
-	}
-
-	public void setStatus(String status) {
-		this.status = status;
-	}
-
-	@Override
-	public String toString() {
-		return ToStringBuilder.reflectionToString(this);
-	}
+    public void setName(String name) {
+        this.name = name;
+    }
 }
---
+++ src/main/webapp/assets/js/trainplan/login.js
@@ -0,0 +1,31 @@
+/**
+ * Created by star on 5/15/14.
+ */
+$(function() {
+    $("#inputUsername").blur(function() {
+        $.ajax({
+            url: "user/" + $("#inputUsername").val().trim() + "/account",
+            method: "GET",
+            contentType: "application/json; charset=UTF-8"
+        }).done(function(list) {
+            $("#inputAccount option").remove();
+            for( var i = 0; i < list.length; i++) {
+                $("#inputAccount").append("<option value='" + list[i].ACC_ID + "'>" + list[i].ACC_NAME + "</option>");
+            }
+        }).fail(function() {
+
+        }).always(function() {
+        })
+    });
+
+    $("#loginForm").submit(function() {
+        if($("#inputUsername").val().trim().length == 0) {
+            return false;
+        }
+        if(!$("#inputAccount").val()) {
+            return false;
+        }
+        $("input[name='username']").val($("#inputUsername").val() + "@" + $("#inputAccount").val());
+        $("input[name='password']").val($("#inputPassword").val());
+    });
+});
