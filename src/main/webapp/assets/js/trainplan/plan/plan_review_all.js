683729b1b06fb680f37a72693209443425f27f38
dengliuiang <dengliujiang@chinacloud.com.cn>
2014-05-28 21:26:19 +0800

    fix bug

--- src/main/webapp/assets/js/trainplan/plan/plan_review_all.js
+++ src/main/webapp/assets/js/trainplan/plan/plan_review_all.js
@@ -54,7 +54,7 @@ var PlanReViewAllPage = function(){
 						var startTotal_Td = 0;//图定始发总合计
 						var startTotal_Lk = 0;//临客始发总合计
 						$.each(result.data,function(n,trainObj){
-							if (trainObj.TDSFXJ !="" && trainObj.TDSFXJ!=" ") {
+							if (trainObj.TDSFXJ !="" && trainObj.TDSFXJ!=" "  && trainObj.TDSFXJ!="undefine") {
 								startTotal_Td += parseInt(trainObj.TDSFXJ);
 							}
 							if (trainObj.TDSFXJX !="" && trainObj.TDSFXJX!=" "  && trainObj.TDSFXJX!="undefine") {
@@ -110,6 +110,7 @@ var PlanReViewAllPage = function(){
 
 						
 						//////////////////////////////////////////////////////
+						console.log("//////////////////////// 总合计 :    图定始发总合计= "+startTotal_Td+"临客始发总合计= "+startTotal_Lk);
 						//总合计
 						startTotal = startTotal_Td+startTotal_Lk;//始发总合计 = 图定始发总合计 + 临客始发总合计
 						if (startTotal ==0) {
