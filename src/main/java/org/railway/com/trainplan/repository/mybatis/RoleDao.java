7703b487266c4a8743bc811ecf6d765dc615601a
dengliuiang <dengliujiang@chinacloud.com.cn>
2014-05-29 11:43:22 +0800

    日计划审批增加权限控制

--- src/main/java/org/railway/com/trainplan/repository/mybatis/RoleDao.java
+++ src/main/java/org/railway/com/trainplan/repository/mybatis/RoleDao.java
@@ -1,8 +1,10 @@
 package org.railway.com.trainplan.repository.mybatis;
 
+import org.railway.com.trainplan.entity.Permission;
 import org.railway.com.trainplan.entity.Role;
 
 import java.util.List;
+import java.util.Map;
 
 /**
  * Created by star on 5/15/14.
@@ -11,4 +13,6 @@ import java.util.List;
 public interface RoleDao {
 
     List<Role> getRoleByAccId(int accId);
+    
+    List<Permission> getPermissionByRoleIds(Map<String, String> queryMap);
 }
