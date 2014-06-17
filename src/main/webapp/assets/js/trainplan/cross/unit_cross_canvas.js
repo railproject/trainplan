6453f09b9eff070d2c19b1b1485fdaee28dac2ac
dengliuiang <dengliujiang@chinacloud.com.cn>
2014-06-17 20:28:02 +0800

    基本交路单元图形增加按组号过滤；绘图组件条件字体等

--- src/main/webapp/assets/js/trainplan/cross/unit_cross_canvas.js
+++ src/main/webapp/assets/js/trainplan/cross/unit_cross_canvas.js
@@ -2,6 +2,8 @@ var _MyCanvas = null;
 
 var canvas = null;
 var context = null;
+var _canvas_select_groupSerialNbr = null;
+var _currentGroupSerialNbr = "";		//当前组号
 
 var currentXScale = 10;	//x轴缩放比例
 var currentXScaleCount = 1;//x轴放大总倍数
@@ -9,7 +11,7 @@ var currentYScale = 1;	//y轴缩放比例
 var currentYScaleCount = 1;//y轴放大总倍数
 var dataIsNull = false;
 var myIsDrawTrainTime = false;	//是否绘制列车经由站到达及出发时间
-
+var pageModel = null;			//knockout绑定对象
 
 var MyCanvas = function(){
 	/**
@@ -20,8 +22,25 @@ var MyCanvas = function(){
 		context.clearRect(0,0,canvas.width,canvas.height);
 	};
 	
+	
+	function initGroupSerialNbrCombox(groupSerialNbrComboxData) {
+		_canvas_select_groupSerialNbr.empty();
+		_canvas_select_groupSerialNbr.append("<option value='' selected='selected'>全部</option>");
+		for(var j=0;j<groupSerialNbrComboxData.length;j++) {
+			if (groupSerialNbrComboxData[j].value == _currentGroupSerialNbr) {
+				_canvas_select_groupSerialNbr.append("<option selected='selected' value='"+groupSerialNbrComboxData[j].value+"'>"+groupSerialNbrComboxData[j].text+"</option>");
+			} else {
+				_canvas_select_groupSerialNbr.append("<option value='"+groupSerialNbrComboxData[j].value+"'>"+groupSerialNbrComboxData[j].text+"</option>");
+			}
+			
+		}
+		
+	};
+	
+	
 	this.drawGraph = function (contextParam, scale) {
-		console.dir(canvasData);
+		var _groupSerialNbrComboxData = [];
+		
 		var booleanDrawJlStartAndEnd = true;	//是否绘制交路起止标记
 		var myCanvasComponent = new MyCanvasComponent(contextParam, canvasData.grid.days, canvasData.grid.crossStns, scale);
 		//1.绘制网格
@@ -32,6 +51,20 @@ var MyCanvas = function(){
 			var _obj = canvasData.jlData[i];
 			var _color = getRandomColor();
 			var _lenJlTrain=_obj.trains.length;
+			
+			//临时保存车组号
+			if (_obj.groupSerialNbr!=null && _obj.groupSerialNbr!="undefine") {
+				_groupSerialNbrComboxData.push({
+					value:_obj.groupSerialNbr,
+					text:myCanvasComponent.convertGroupSerialNbr(_obj.groupSerialNbr)+"组"
+				});
+			}
+			
+			
+			if (scale && scale.currentGroupSerialNbr && scale.currentGroupSerialNbr!="undefine"
+				&&scale.currentGroupSerialNbr!="" && scale.currentGroupSerialNbr!=_obj.groupSerialNbr) {
+				continue;
+			}
 			
 			//2.1 绘制交路列车运行线
 			for (var j=0; j<_lenJlTrain; j++) {
@@ -49,20 +82,26 @@ var MyCanvas = function(){
 			
 		}
 		
+		
+		//3.渲染组号下拉框数据
+		initGroupSerialNbrCombox(_groupSerialNbrComboxData);
+		
 	};
+	
+	
 	
 };
 
 
 
+
 $(function(){
-	
 	if(canvasData==null || canvasData.grid==null) {
 		showErrorDialog("当前不存在列车数据！");
 		dataIsNull = true;
 		return;
 	}
-	
+	_canvas_select_groupSerialNbr = $("#canvas_select_groupSerialNbr");
 	canvas = document.getElementById("unit_cross_canvas");
 	context = canvas.getContext('2d');
 
@@ -71,7 +110,19 @@ $(function(){
 	
 	
 	
-	//是否绘制列车经由站到达及出发时间
+	$("#canvas_select_groupSerialNbr").change(function(){
+		_currentGroupSerialNbr = _canvas_select_groupSerialNbr.val();
+		
+		_MyCanvas.drawGraph(context,{
+			isDrawTrainTime:myIsDrawTrainTime,			//是否绘制列车经由站到达及出发时间
+			xScale : currentXScale,						//x轴缩放比例
+			xScaleCount : currentXScaleCount,			//x轴放大总倍数
+			yScale : currentYScale,						//y轴缩放比例
+			currentGroupSerialNbr : _currentGroupSerialNbr	//当前组号
+		});
+	});
+	
+	//显示停站时刻 复选框事件
 	$("#canvas_checkbox_trainTime").click(function(){
 		if (dataIsNull) {
 			showErrorDialog("当前不存在列车数据！");
@@ -155,6 +206,7 @@ $(function(){
 			 yScale : currentYScale			//y轴缩放比例
 		 });
 	});
+	
 	//y放大2倍
 	$("#canvas_event_btn_y_magnification").click(function(){
 		if (currentYScaleCount == 8) {
@@ -208,8 +260,8 @@ $(function(){
 		 });
 	});
 	
+	
 });
-
 
 
 
