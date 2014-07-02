e26adc793186df9562620fc95300c46af073b282
cdlilin <llaysz@163.com>
2014-07-02 11:40:17 +0800

    fix

--- src/main/webapp/assets/js/trainplan/highLine/highLine_cross.js
+++ src/main/webapp/assets/js/trainplan/highLine/highLine_cross.js
@@ -212,11 +212,11 @@ function CrossModel() {
 						 self.selectedHighLineCrossRows.remove(currentCorss);
 						 self.acvtiveHighLineCrosses.push(cross); 
 					} else {
-						showErrorDialog("获取运行规律失败");
+						showErrorDialog("没有加载的交路数据");
 					} 
 				},
 				error : function() {
-					showErrorDialog("接口调用失败");
+					showErrorDialog("加载的交路数据失败");
 				},
 				complete : function(){
 					commonJsScreenUnLock();
